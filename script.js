71cf7fd00d4626b1ca3fc0d23643a61519e8e98a
Vansh Sikka <141615610+vanshsikka1222@users.noreply.github.com>
2023-09-16 11:52:27 +0530

    JavaScript file

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    const url = 'https://vanshsikka.notion.site/vanshsikka/Hi-I-m-Vansh-4a39f527af8a4e23b1ae605530b4d043';
+
+    window.open(url, '_blank');
+});
+
+document.getElementById('downloadButton').addEventListener('click', function() {
+    const url = 'https://drive.google.com/file/d/1AM0XlqCrDUTfR9NL_-bunMeebyWScBTj/view?usp=sharing';
+
+    window.open(url, '_blank');
+});
+  
